f08f889c0d45cfd58c934f6de4abdb2bff1387cf
anz-28 <anshudharmashahary@gmail.com>
2026-03-29 15:40:04 +0530

    analyticsfix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './App.css';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
